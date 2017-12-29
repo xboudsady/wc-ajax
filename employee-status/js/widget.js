1f37d57b413f85d42d928fd50776afd51b2f9bc9
Xayaseth Boudsady <boudsadyx@gmail.com>
2017-12-29 13:07:24 -0800

    Added for loop to cycle through JSON data to add an <li> element classs of inoffice status. Data will dynamically generate html.

--- employee-status/js/widget.js
+++ employee-status/js/widget.js
@@ -3,7 +3,18 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
         var employees = JSON.parse(xhr.responseText);
-        console.log(employees);
+        var statusHTML = '<ul class="bulleted">';
+        for (var i = 0; i < employees.length; i += 1) {
+            if (employees[i].inoffice === true) {
+                statusHTML += '<li class="in">';
+            } else {
+                statusHTML += '<li class="out">';
+            }
+            statusHTML += employees[i].name;
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 
